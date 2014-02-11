27bbd035b87bb65734a2591f75f9be056e840b20
rafaelrf500 <rafaelrf500@gmail.com>
2014-02-11 23:23:48 +0000

    Atualizado

---
+++ org.xtext.myModel/src-gen/org/xtext/example/myModel/serializer/CSharpSemanticSequencer.java
@@ -0,0 +1,42 @@
+package org.xtext.example.myModel.serializer;
+
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.xtext.serializer.acceptor.ISemanticSequenceAcceptor;
+import org.eclipse.xtext.serializer.diagnostic.ISemanticSequencerDiagnosticProvider;
+import org.eclipse.xtext.serializer.diagnostic.ISerializationDiagnostic.Acceptor;
+import org.eclipse.xtext.serializer.sequencer.AbstractDelegatingSemanticSequencer;
+import org.eclipse.xtext.serializer.sequencer.GenericSequencer;
+import org.eclipse.xtext.serializer.sequencer.ISemanticSequencer;
+import org.eclipse.xtext.serializer.sequencer.ITransientValueService;
+import org.xtext.example.myModel.cSharp.CSharpPackage;
+import org.xtext.example.myModel.cSharp.Model;
+import org.xtext.example.myModel.services.CSharpGrammarAccess;
+
+@SuppressWarnings("all")
+public class CSharpSemanticSequencer extends AbstractDelegatingSemanticSequencer {
+
+	@Inject
+	private CSharpGrammarAccess grammarAccess;
+	
+	public void createSequence(EObject context, EObject semanticObject) {
+		if(semanticObject.eClass().getEPackage() == CSharpPackage.eINSTANCE) switch(semanticObject.eClass().getClassifierID()) {
+			case CSharpPackage.MODEL:
+				if(context == grammarAccess.getModelRule()) {
+					sequence_Model(context, (Model) semanticObject); 
+					return; 
+				}
+				else break;
+			}
+		if (errorAcceptor != null) errorAcceptor.accept(diagnosticProvider.createInvalidContextOrTypeDiagnostic(semanticObject, context));
+	}
+	
+	/**
+	 * Constraint:
+	 *     input+=Input*
+	 */
+	protected void sequence_Model(EObject context, Model semanticObject) {
+		genericSequencer.createSequence(context, semanticObject);
+	}
+}
